66010bc0f28572b0f4f98e93cfd26944ee54f45e
Ben <ben399399@naver.com>
2019-04-21 23:08:35 +0900

    Added Archive area to home

--- lib/home.dart
+++ lib/home.dart
@@ -8,6 +8,7 @@ import 'model/task.dart';
 import 'bloc/task_bloc.dart';
 import 'component/task_list_item.dart';
 import 'component/task_list_item_done.dart';
+import 'component/task_list_item_archive.dart';
 
 class HomeWidget extends StatefulWidget {
   @override
@@ -43,20 +44,22 @@ class _HomeWidgetState extends State<HomeWidget> {
                       ),
                     ),
                     SizedBox(height: 12),
-                    (bloc as List<Task>).where((x) => !x.isDone).isEmpty
+                    (bloc as List<Task>).where((x) => !x.isDone&&!x.isArchived).isEmpty
                         ? Padding(
                             padding: EdgeInsets.only(left: 32),
                             child: Text("오늘 할일을 다 끝냈어요"))
                         : ListView.builder(
                             itemCount: (bloc as List<Task>)
-                                .where((x) => !x.isDone)
+                                .where((x) => !x.isDone && !x.isArchived)
                                 .length,
                             shrinkWrap: true,
                             itemBuilder: (context, index) => TaskListItem(
-                                task: (bloc as List<Task>)
-                                    .where((x) => !x.isDone)
-                                    .toList()[index],
-                                repaint: repaint),
+                                  task: (bloc as List<Task>)
+                                      .where((x) => !x.isDone && !x.isArchived)
+                                      .toList()[index],
+                                  repaint: repaint,
+                                  parentScaffold: _scaffoldKey,
+                                ),
                           ),
                     SizedBox(
                       height: 32,
@@ -64,6 +67,133 @@ class _HomeWidgetState extends State<HomeWidget> {
                   ],
                 );
               },
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child: InkWell(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(32),
+                      topRight: Radius.circular(32)),
+                  onTap: () {
+                    (_scaffoldKey.currentState as ScaffoldState)
+                        .showBottomSheet((context) {
+                      return Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 16),
+                        child: BottomSheet(
+                          builder: (context) => Material(
+                                elevation: 4,
+                                borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(32),
+                                    topRight: Radius.circular(32)),
+                                child: Container(
+                                  width: MediaQuery.of(context).size.width - 32,
+                                  height:
+                                      MediaQuery.of(context).size.height - 32,
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.only(
+                                          topLeft: Radius.circular(32),
+                                          topRight: Radius.circular(32)),
+                                      color: Colors.blue),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                      SizedBox(
+                                        height: 64,
+                                      ),
+                                      Padding(
+                                        padding:
+                                            const EdgeInsets.only(left: 24),
+                                        child: Text(
+                                          "ARCHIVE",
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .title
+                                              .copyWith(
+                                                  color: Colors.white
+                                                      .withOpacity(0.89)),
+                                        ),
+                                      ),
+                                      Column(
+                                        children: <Widget>[
+                                          BlocBuilder(
+                                            bloc: _taskBloc,
+                                            builder: (context, bloc) => (bloc
+                                                            as List<Task>)
+                                                        .where(
+                                                            (x) => x.isArchived)
+                                                        .length >
+                                                    0
+                                                ? ListView.builder(
+                                                    itemCount: (bloc
+                                                            as List<Task>)
+                                                        .where(
+                                                            (x) => x.isArchived)
+                                                        .length,
+                                                    shrinkWrap: true,
+                                                    itemBuilder: (context,
+                                                            index) =>
+                                                        TaskListItemArchive(
+                                                          task: ((bloc
+                                                                  as List<Task>)
+                                                              .where((x) =>
+                                                                  x.isArchived)
+                                                              .toList())[index],
+                                                          parentScaffold:
+                                                              _scaffoldKey,
+                                                          repaint: repaint,
+                                                        ))
+                                                : Container(),
+                                          ),
+                                          IconButton(
+                                              icon: Icon(
+                                                Icons.arrow_drop_down,
+                                                color: Colors.white
+                                                    .withOpacity(0.89),
+                                              ),
+                                              onPressed: () =>
+                                                  Navigator.pop(context))
+                                        ],
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ),
+                          onClosing: () {},
+                        ),
+                      );
+                    });
+                  },
+                  child: Container(
+                    height: 96,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(32),
+                            topRight: Radius.circular(32)),
+                        color: Colors.blue),
+                    child: Transform.translate(
+                      offset: Offset(24, -20),
+                      child: Row(
+                        children: <Widget>[
+                          Icon(
+                            Icons.archive,
+                            color: Colors.white,
+                          ),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          Text("ARCHIVE",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold))
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
             ),
             Align(
               alignment: Alignment.bottomCenter,
@@ -163,12 +293,30 @@ class _HomeWidgetState extends State<HomeWidget> {
                             topLeft: Radius.circular(32),
                             topRight: Radius.circular(32)),
                         color: Colors.amber),
+                    child: Transform.translate(
+                      offset: Offset(24, 0),
+                      child: Row(
+                        children: <Widget>[
+                          Icon(
+                            Icons.check,
+                            color: Colors.white,
+                          ),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          Text("DONE",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold))
+                        ],
+                      ),
+                    ),
                   ),
                 ),
               ),
             ),
             Padding(
-              padding: const EdgeInsets.only(bottom: 16, left: 250),
+              padding: const EdgeInsets.only(bottom: 20, left: 225),
               child: Align(
                 alignment: Alignment.bottomCenter,
                 child: Transform.rotate(
